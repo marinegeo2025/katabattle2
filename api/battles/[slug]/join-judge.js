1a83d6ad7e73d7bb21f069a82bb321806487d18a
marinegeo2025 <barnardalex@gmail.com>
2025-10-07 13:03:55 +0100

    Update join-judge.js

--- api/battles/[slug]/join-judge.js
+++ api/battles/[slug]/join-judge.js
@@ -1,5 +1,5 @@
 // api/battles/[slug]/join-judge.js
-import { loadBattle, saveBattle, randToken, sha256 } from "../../_utils.js";
+import { loadBattle, saveBattle, randToken, sha256 } from "../../../_utils.js";
 
 export default async function handler(req, res) {
   const { slug } = req.query;
